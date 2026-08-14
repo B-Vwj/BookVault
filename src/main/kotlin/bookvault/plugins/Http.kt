633113e4b66fdf18ba106c5fc2475ebedf9e9370
Brandon Vue <bvwj1231@gmail.com>
2026-08-14 13:03:38 -0500

    fix: re-add hosts

--- src/main/kotlin/bookvault/plugins/Http.kt
+++ src/main/kotlin/bookvault/plugins/Http.kt
@@ -16,9 +16,8 @@ fun Application.configureHttp() {
         allowHeader(HttpHeaders.Authorization)
         allowHeader(HttpHeaders.ContentType)
 //        allowCredentials = true
-//        allowHost("temporary-racing-zinc-s7x14qf.vercel.app", schemes = listOf("http", "https"))
-//        allowHost("book-vault-b-vwjs-projects.vercel.app", schemes = listOf("http", "https"))
-//        allowHost("localhost:3000", schemes = listOf("http"))
-        anyHost()
+        allowHost("temporary-racing-zinc-s7x14qf.vercel.app", schemes = listOf("http", "https"))
+        allowHost("book-vault-b-vwjs-projects.vercel.app", schemes = listOf("http", "https"))
+        allowHost("localhost:3000", schemes = listOf("http"))
     }
 }
